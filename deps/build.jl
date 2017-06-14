6135b34f6f095dea6645ca3c73c143676d19e95e
Mykel Kochenderfer <m.kochenderfer@gmail.com>
2017-06-14 09:25:22 -0700

    remove redundant code in build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -5,9 +5,6 @@ end
 if !in("Records", packages)
     Pkg.clone("https://github.com/tawheeler/Records.jl.git")
 end
-if !in("Records", packages)
-    Pkg.clone("https://github.com/tawheeler/Records.jl.git")
-end
 if !in("AutomotiveDrivingModels", packages)
 	Pkg.clone("https://github.com/sisl/AutomotiveDrivingModels.jl.git")
 end
